f5e1770f3309174675375ab966c186a5f91b383b
marco.madalin.farcas <marco.madalin.farcas@est.fib.upc.edu>
2022-11-17 18:59:45 +0100

    Exercise review fragment UI

--- app/src/main/java/com/marcomadalin/olympus/presentation/view/ExerciseReviewFragment.kt
+++ app/src/main/java/com/marcomadalin/olympus/presentation/view/ExerciseReviewFragment.kt
@@ -5,15 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.marcomadalin.olympus.R
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import com.marcomadalin.olympus.databinding.FragmentExerciseReviewBinding
+import com.marcomadalin.olympus.presentation.viewmodel.ExerciseDataViewModel
 
 class ExerciseReviewFragment : Fragment() {
+
+    private var _binding : FragmentExerciseReviewBinding? = null
+    private val binding get() = _binding!!
+
+    private val exerciseDataViewModel : ExerciseDataViewModel by activityViewModels()
+
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_exercise_review, container, false)
+        _binding = FragmentExerciseReviewBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+
+    }
 }
